96af79e306677f78aa67148e5f697d88a1451775
IntFelix2024 <felix.steiner@hak-reutte.ac.at>
2024-01-30 12:52:54 +0100

    Renaming of classes Test and Post to match syntax

--- src/socialNetwork/Test.java
+++ src/socialNetwork/Test.java
@@ -1,6 +1,6 @@
 package socialNetwork;
 
-public class test {
+public class Test {
 
 	public static void main(String[] args) {
 
--- src/socialNetwork/post.java
+++
@@ -1,5 +0,0 @@
-package socialNetwork;
-
-public abstract class post {
-
-}
