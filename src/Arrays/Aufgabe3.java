1631799d348ba6ad6dd3808aff844bc611df0ca2
IntFelix2024 <felix.steiner@hak-reutte.ac.at>
2024-02-26 16:02:23 +0100

    Start of Aufgabe3

---
+++ src/Arrays/Aufgabe3.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.awt.geom.Point2D;
+
+public class Aufgabe3 {
+	
+	double minDistance(Point[] points, int size) {
+		for(int i = 0; i < points.length; i++) {
+			if(distance( points[i].x,  points[i].y,  0,  0)) {
+				
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Point[] points = { new Point(10,20), new Point(12,2), new Point(44, 4) };
+
+	}
+
+}
