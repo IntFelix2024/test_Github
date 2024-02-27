9a8cd3e8dba8c0a5dba78df675aebc736edc48d3
IntFelix2024 <felix.steiner@hak-reutte.ac.at>
2024-02-27 13:25:28 +0100

    class Augabe4 added, unfinished, to be continued

---
+++ src/Arrays/Aufgabe4.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Aufgabe4 {
+	
+	public static int[][]magnify(int[][]array, int factor){
+		int magnify[][] = new int [array.length*factor][array[0].length*factor];
+		for(int x = 0; x < array.length; x++) {
+			for(int y = 0; y < array[0].length; y++) {
+				magnify[x*factor][y*factor] = array[x][y];
+				System.out.println(magnify[x][y]);
+			}
+		}
+		return magnify;
+	}
+
+	public static void main(String[] args) {
+		int numbers[][] = { {1,2,3}, {4,5,6}, {7,8,9} };
+		magnify(numbers, 2);
+	}
+
+}
