6f7854773525372c6af0c90fc21bfaed750a9957
IntFelix2024 <felix.steiner@hak-reutte.ac.at>
2024-03-05 13:34:21 +0100

    Aufgaben cmopleted

--- src/Arraylist/Aufgabe1.java
+++ src/Arraylist/Aufgabe1.java
@@ -1,5 +1,32 @@
 package Arraylist;
 
-public class Aufgabe1 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Aufgabe1 {
+	
+	public static void reduceToComments(List<String>lines) {
+		if(lines.size()%4 == 0) {
+		for(int i = 0; i < lines.size()-3; i++) {
+			lines.remove(i);
+			lines.remove(i);
+			lines.remove(i);
+		}
+		System.out.println(lines);
+		} else {
+			System.out.println("Nicht durchführbar");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> test = new ArrayList<String>();
+		for(int i = 0; i < 20; i++){
+		test.add("k");
+		test.add("v");
+		test.add("w");
+		test.add("ko");
+		}
+		reduceToComments(test);
+	}
 }
--- src/Arrays/Aufgabe3.java
+++ src/Arrays/Aufgabe3.java
@@ -8,11 +8,11 @@ public class Aufgabe3 {
 	
 	double minDistance(Point[] points, int size) {
 		for(int i = 0; i < points.length; i++) {
-			if(distance( points[i].x,  points[i].y,  0,  0)) {
+//			if(distance( points[i].x,  points[i].y,  0,  0)) {
 				
 			}
 		}
-	}
+//}
 
 	public static void main(String[] args) {
 		
