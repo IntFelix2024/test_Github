d87be64e606e7a277e2310bf290211e9bf98feeb
IntFelix2024 <felix.steiner@hak-reutte.ac.at>
2024-04-22 15:14:33 +0200

    Fibonacci added

--- src/recursion/Fibonacci.java
+++ src/recursion/Fibonacci.java
@@ -2,43 +2,48 @@ package recursion;
 
 public class Fibonacci {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		public static int fibonacciIterative (int n) {
-			if (n <= 1) {
+	public static int fibonacciIterative (int n) {
+		if (n <= 1) {
 			return n;
-			} else {
+		} else {
 			int fib0 = 0;
 			int fib1= 1;
 			for (int i = 2; i <= n; i++) {
-			int temp = fib1;
-			fib1 += fib0;
-			fib0 = temp;
-
+				int temp = fib1;
+				fib1 += fib0;
+				fib0 = temp;
+			}	
 			return fib1;
-
-			}
 		}
+
 	}
 		
-		public static int fibonacciArray (int n) {
-			if (n <= 1) {
+	public static int fibonacciArray (int n) {
+		if (n <= 1) {
 			return n;
-			} else {
+		} else {
 			int[] fib = new int[n + 1];
 			fib[0]= 0;
 			fib[1] = 1;
 			for (int i = 2; i <= n; i++) {
-			fib[i] = fib[i - 1] + fib[i - 2];
+				fib[i] = fib[i - 1] + fib[i - 2];
 			}
 		return fib[n];
-			}
 		}
+	}
 		
 		public static int fibonacciRecursive (int n) {
 			if (n <= 1) {
-			return n;
+				return n;
 			}
 			return fibonacciRecursive(n- 1) + fibonacciRecursive(n- 2);
 		}
+
+
+public static void main(String[] args) {
+	int n = 50;
+	System.out.println(fibonacciIterative(n));
+	System.out.println(fibonacciRecursive(n));
+	System.out.println(fibonacciArray(n));
+}
 }
